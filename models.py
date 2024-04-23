9d1a7be107c0f3e4e3725e8decec2e9cb8c83545
RoshanJaishi <81689673+RoshanJaishi@users.noreply.github.com>
2024-04-23 18:20:46 +0545

    Update models.py

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 # models.py
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
@@ -21,27 +21,4 @@ class Story(db.Model):
             'content': self.content,
             'date_created': self.date_created.isoformat()  # ISO formatted date
         }
-=======
-# models.py
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 
-db = SQLAlchemy()  # Initialize the SQLAlchemy instance
-
-class Story(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80), nullable=False)
-    content = db.Column(db.Text, nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return f"<Story {self.title}>"
-
-    def serialize(self):
-        return {
-            'id': self.id,
-            'title': self.title,
-            'content': self.content,
-            'date_created': self.date_created.isoformat()  # ISO formatted date
-        }
->>>>>>> 1430fc7d95c22442980aa8138ad75bdfe119a5b0
