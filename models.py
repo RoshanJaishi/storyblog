1430fc7d95c22442980aa8138ad75bdfe119a5b0
RoshanJaishi <81689673+RoshanJaishi@users.noreply.github.com>
2024-04-23 17:42:34 +0545

    Add files via upload

---
+++ models.py
@@ -0,0 +1,22 @@
+# models.py
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()  # Initialize the SQLAlchemy instance
+
+class Story(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<Story {self.title}>"
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'content': self.content,
+            'date_created': self.date_created.isoformat()  # ISO formatted date
+        }
